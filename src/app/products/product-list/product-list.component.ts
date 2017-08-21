46fd422b1c920b5d00377d59ca362bfe2daa15c4
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-08-21 11:07:55 -0400

    Filtering a List
    NOTE: "The Angular team and many experienced Angular developers strongly recommend moving filtering and sorting logic into the component itself." [rather than using a custom pipe as previous versions of this course may have demonstrated].  "Any capabilities that you would have put in a pipe and shared across the app can be written in a filtering/sorting service and injected into the component."
    (https://angular.io/guide/pipes#appendix-no-filterpipe-or-orderbypipe)
    
    - Change listFilter property into a getter and setter.

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -13,7 +13,19 @@ export class ProductListComponent implements OnInit {
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
-  listFilter = 'cart';
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    // If we want to perform some logic every time this value is changed, we can add it here within the setter function.
+    // Set the filteredProducts array to the filtered list of products using a JS conditional operator to handle the possibility that the listFilter string is empty, null or undefined - returning the complete products array in that case.
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: Product[] = [];
 
   products: Product[] = [
     {
@@ -68,7 +80,11 @@ export class ProductListComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor() {
+    // Set defaults.
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
 
   ngOnInit() {
     console.log('in OnInit');
@@ -76,6 +92,15 @@ export class ProductListComponent implements OnInit {
 
   toggleImage() {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): Product[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    // Use the array.prototype.filter() method to provide a new array of elements that pass the included test.
+    // ES5:
+    //    return this.products.filter((product: Product) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    // ES2016:
+    return this.products.filter((product: Product) => product.productName.toLocaleLowerCase().includes(filterBy));
   }
 
 }
