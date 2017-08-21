c4c113228422865a166dcbcde0f57210b42ec35b
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-08-21 18:06:03 -0400

    Shared Module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
-
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductService } from './product.service';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
@@ -12,8 +9,6 @@ import { ProductGuard } from './product.guard';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StarComponent } from './star/star.component';
 import { ConvertToSpacesPipe } from './convert-to-spaces.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [StarComponent, ConvertToSpacesPipe],
-  exports: [StarComponent, ConvertToSpacesPipe]
+  exports: [
+    CommonModule,
+    FormsModule,
+    StarComponent,
+    ConvertToSpacesPipe,
+  ]
 })
 export class SharedModule { }
