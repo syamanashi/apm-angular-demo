1e3ede7e3b356ba246be4ed60efcb6da0db2d62e
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-08-21 16:04:37 -0400

    Navigation and Routing Basics: Configuring Routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'welcome', component: HomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }, // 404PageComponent goes here.
 ];
 
 @NgModule({
